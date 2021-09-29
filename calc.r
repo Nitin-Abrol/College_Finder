b3b8d4889e4e6338c21f4771c8f5e3179fea0246
Nitin Abrol <abroln39@gmail.com>
2021-09-29 20:41:55 +0530

    Create calc.r

---
+++ calc.r
@@ -0,0 +1,26 @@
+#Simple Calculator Using R
+#Enter numbers of Your Own Choice
+
+num1<- Enter choice 1
+num2<- Enter choice 2
+
+#Function for addition
+num3=cat("Addition : ",num1+num2)
+
+#Function for Substraction
+num3=cat("Substraction : ",num1-num2)
+
+#Function for Multiplication
+num3=cat("Multiplication : ",num1*num2)
+
+#Function for Division
+num3=cat("Division : ",num1/num2)
+
+#Function Raised-to operation
+num3=cat("Raised-to Operation : ",num1^num2)
+
+#Function for Remainder
+num3=cat("Remainder : ",num1%%num2)
+
+#Function for Integer of Division
+num3=cat("Integer of Division :  ",num1%/%num2)
